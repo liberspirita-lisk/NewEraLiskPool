ddbc1644641356f1c3d0cd36690d767643f16f18
liberspirita-lisk <jh@ojura.net>
2019-12-31 13:19:55 +0100

    nouveau fichier : config 	modifié :         import_sqlite.sql 	modifié :         pgsql2sqlite.sh

--- import_sqlite.sql
+++ import_sqlite.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS voters(
 	     username TEXT,
 	     address TEXT UNIQUE ,
 	     publickey TEXT UNIQUE,
-	     power INTEGER,
+	     scoring INTEGER,
 	     pending_payouts REAL DEFAULT 0.0);
 
 CREATE TABLE IF NOT EXISTS delegates(
@@ -21,14 +21,14 @@ DELETE FROM delegates ;
 .separator ,
 .import delegates.csv delegates
 
-INSERT or IGNORE INTO voters( delegate, username, address,publickey,power)
+INSERT or IGNORE INTO voters( delegate, username, address,publickey,scoring)
 	SELECT delegate, username,address,publickey,power FROM voters_raw ;
 
 DELETE FROM voters where  address not in (select address from voters_raw);
 
-update voters set pending_payouts = pending_payouts+ ((power / (select total(power) from voters)) * ((select total(balance) from delegates where address='2004134067472288525L')-(select total(pending_payouts) from voters )));
+UPDATE voters_raw SET power = min(power,100000*100000000) ;
+UPDATE voters SET scoring = (SELECT total(voters_raw.power) from voters_raw WHERE voters_raw.address=voters.address GROUP BY voters_raw.address) ;
 
-UPDATE voters SET power = (SELECT total(voters_raw.power) from voters_raw WHERE voters_raw.address=voters.address GROUP BY voters_raw.address) ;
+update voters set pending_payouts = pending_payouts+ ((scoring / (select total(scoring) from voters)) * ((select total(balance) from delegates where address='2004134067472288525L')-(select total(pending_payouts) from voters ))); 
 
-/*DELETE FROM voters_raw ;*/
-/*UPDATE voters_raw SET delegate= (select username FROM delegates WHERE voters_raw.delegate=delegates.publickey);*/
+
--- pgsql2sqlite.sh
+++ pgsql2sqlite.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# ***** Modifier les paramètres ci-dessous *****
-
-path_pool='/home/lisk/pool/' 				# Chemin absolu vers la pool
-path_psql='/home/lisk/lisk-main/pgsql/bin/' 		# Chemin absolu executables postgresql de lisk
-
-# les delegates sur lesquels on recherche les votants, on peut en mettre tant qu'on veut, pour sponsorer des potes
-declare -a pubkey=('32f20bee855238630b0f791560c02cf93014977b4b25c19ef93cd92220390276' 'de918e28b554600a81cbf119abf5414648b58a8efafbc3b0481df0242684dc1b' '6b221c994ba2a69d8893f59a55431dd3e94327830f8818fc7fbffdbbb55b6fa9')
-
-taulier='7695675674841127110L'			# Adresse du patron de la pool. Ce compte doit voter la pool pour que ça marche
-tx_redistribution='60'				#taux de redistribution ex: 60 = 60 aux poolers, 40% au taulier
-min_balance='10000000000' 			# Solde minimum des votants à comptabiliser. Pour éviter les morts ou dormants.
-
-# ***** ne rien modifier ensuite, sauf si vous savez ce que vous faites *****
+source config
 touch $patch_pool"voters.csv"
 touch $patch_pool"delegates.csv"
 cd $path_pool
@@ -32,9 +20,8 @@ $path_psql"psql" -d lisk_main -t -A -F"," -c "select \"username\",\"address\",\"
 done
 
 # Reprise de respiration
-sleep 1
-sync
-sync
+sync;sync
+
 # import dans SQLITE3 base pool.db3
 # et pleins de traitements
 /usr/bin/sqlite3 $path_pool"pool.db3" < $path_pool"import_sqlite.sql"
