ac72d504ea1057c431e55ec52f508f1a9e8412ce
liberspirita-lisk <jh@ojura.net>
2020-01-03 13:45:41 +0000

    modifié :         bash_engine 	supprimé :        public/index.php

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = new SQLite3('../db_pool');
-$res = $db->query('SELECT address, round((pending_payouts/100000000),2) pending_payouts from voters limit 30; ');
-while ($row = $res->fetchArray()) {
-    echo "{$row['address']} {$row['pending_payouts']} \n";
-}
-?>
-
