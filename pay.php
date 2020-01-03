0f9c88522b5eb1b5a0d010b1cf5b79ca2eda53a8
liberspirita-lisk <jh@ojura.net>
2020-01-03 22:45:06 +0000

    modifié :         bash_engine 	modifié :         config 	modifié :         pay.php

--- pay.php
+++ pay.php
@@ -5,7 +5,7 @@ $db = new SQLite3('db_pool');
 $conf = $db->query('SELECT * from config limit 1; ');
 $conf_row= $conf->fetcharray();
 $res = $db->query("SELECT address, cast(pending_payouts as int) pending_payouts from voters where pending_payouts>". $conf_row['payout_seuil']." ; ");
-if ($conf_row['rank']<102) {
+if ($conf_row['rank']<=101) {
 while ($row = $res->fetchArray())  	{
 	$payout=$row['pending_payouts']-$fee;
 	$withdraw=CreateTransaction("{$row['address']}","$payout","{$conf_row['secret1']}",false,false,-10);
@@ -13,14 +13,14 @@ while ($row = $res->fetchArray())  	{
 	$send=SendTransaction(json_encode($withdraw),$server);
 
 	$db->query("UPDATE voters set pending_payouts=pending_payouts-'".$row['pending_payouts']."' WHERE address ='".$row['address']."'; ");
-	echo      ("UPDATE voters set pending_payouts=pending_payouts-'".$row['pending_payouts']."' WHERE address ='".$row['address']."'; \t");
+	echo      ("Paying '".$row['pending_payouts']."' to '".$row['address']."'; \r");
 
 	$db->query("UPDATE config set current_balance=current_balance-'".$row['pending_payouts']."'; ");
-	echo      ("UPDATE config set current_balance=current_balance-'".$row['pending_payouts']."'; \r");
 
 }} else {
-echo $conf_row['rank']."\n";
+echo "No payout till not forging, current rank = ".$conf_row['rank']."\n";
 	}
+
 $db->close();
 ?>
 
