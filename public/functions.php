b23bc333e873c7e892bd8f38d86df479ee7c12c3
liberspirita-lisk <jh@ojura.net>
2020-01-11 10:39:22 +0000

    modifié :         functions.php

--- public/functions.php
+++ public/functions.php
@@ -36,7 +36,6 @@ $db = new SQLite3('../db_pool');
 $conf = $db->query('SELECT * from config limit 1; ');
 $config= $conf->fetcharray();
 $h = $db->query('SELECT total(pending_payouts) pending_payouts from voters ');
-$z = $db->query('SELECT total(pending_payouts) pending_payouts from voters ');
 $stat_page="<TABLE border=4px cellpadding=15 align=center><TR><TH>Heroes</TH><TH>Zeroes</TH></TR>";
 $stat_page = $stat_page."<TR><TD><TABLE>";
 $h = $db->query('SELECT * from heroes where rank >= 1 order by rank');
@@ -62,7 +61,7 @@ $v = $db->query("SELECT * from voters where address != '".$config['address_reven
 $stat_page="<TABLE border=2px cellpadding=3><TR><TH>Address</TH><TH>  Scoring  </TH><TH>lisk pending payout</TH></TR>";
 
 while ($voters= $v->fetcharray()) {
-	$stat_page = $stat_page."<TR><TD>".$voters['address']."</TD><TD align=right>".number_format($voters['scoring']/10000000000,0) ."</TD><TD align=right>".number_format($voters['pending_payouts']/100000000,3) ."</TD></TR>";
+	$stat_page = $stat_page."<TR><TD><a href=\"vote_report.php?a=".$voters['address']."\">".$voters['address']."</a> </TD><TD align=right>".number_format($voters['scoring']/10000000000,0) ."</TD><TD align=right>".number_format($voters['pending_payouts']/100000000,3) ."</TD></TR>";
 
 }
 $stat_page = $stat_page."</TABLE>";
@@ -70,4 +69,42 @@ echo $stat_page;
 
 }
 
+
+function vote_report()
+{	$db = new SQLite3('../db_pool');
+	$address=$_GET['a'];
+	echo "<h2>Votes report for address ".$address."<br></h2>";
+//$sql = $db->query("SELECT * from voters_raw where address='$address' ; ");
+//$votes= $sql->fetcharray();
+//		while ($votes= $v-> fetcharray()) {}
+
+$conf = $db->query('SELECT * from config limit 1; ');
+$config= $conf->fetcharray();
+$h = $db->query('SELECT total(pending_payouts) pending_payouts from voters ');
+$stat_page="<TABLE border=4px cellpadding=15 align=center><TR><TH bgcolor=green>Votes on Heroes<br>perfect if all green</TH><TH bgcolor=red>Votes on Zeroes<br>perfect if all white</TH></TR>";
+$stat_page = $stat_page."<TR><TD><TABLE>";
+$h = $db->query('SELECT substr(publickey,3,100) publickey, address, rank, username from heroes where rank >= 1 order by rank');
+while ($heroes= $h->fetcharray()) {
+	
+		$sql = $db->query("SELECT count(*) from voters_raw where delegate='".$heroes['publickey']."' and address='$address' ; ");
+		$count_votes= $sql->fetcharray();
+		if ($count_votes[0]>0) {$color='green';} else { $color='white';}
+	        $stat_page = $stat_page."<TR><TD bgcolor=$color>#".$heroes['rank']." ".$heroes['username']."</TD></TR>";
+		                }
+$stat_page = $stat_page."</TABLE>";
+$stat_page = $stat_page."<TD><TABLE>";
+
+
+$z = $db->query('SELECT substr(publickey,3,100) publickey, address, rank, username from zeroes where rank <= 202 order by rank');
+while ($zeroes= $z->fetcharray()) {
+		$sql = $db->query("SELECT count(*) from voters_raw where delegate='".$zeroes['publickey']."' and address='$address' ; ");
+		$count_votes= $sql->fetcharray();
+		if ($count_votes[0]>0) {$color='red';} else { $color='white';}
+	        $stat_page = $stat_page."<TR><TD bgcolor=$color>#".$zeroes['rank']." ".$zeroes['username']."</TD></TR>";
+		                }
+$stat_page = $stat_page."</TABLE>";
+$stat_page = $stat_page."</TABLE>";
+echo $stat_page;
+
+}
 ?>
