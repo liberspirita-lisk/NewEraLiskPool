937da9321b567a66cc90adeb768633d9f1490a82
liberspirita-lisk <jh@ojura.net>
2020-01-11 10:39:52 +0000

    nouveau fichier : vote_report.php

---
+++ public/vote_report.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>New Era liberspirita lisk pool</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div>
+			<ul id="navigation">
+				<li class="active">
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="stats.php">Figures</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+        <div id="header">
+                <div class="clearfix">
+			<img src="images/logo.png" alt="Img" height="100" width="250">
+			<h1> 100% ready for LIP23 !</h1>
+                </div>
+        </div>
+
+	<div id="features" align=center>
+		<div >
+			<h1>Vote report</h1>
+		<div>
+		</html> <?php
+require('functions.php');
+vote_report();
+?> <html>
+</body>
+</html>
